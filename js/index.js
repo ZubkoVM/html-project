1d24dd5af2c42e29eea3d6a037f9b6cece71d534
ZubkoVM <Zubko_V@mail.ru>
2020-03-22 16:55:09 +0000

    Comment

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 const clockPlaceholder = document.querySelector('.clock');
 setInterval( () => clockPlaceholder.innerText = (new Date()).toLocaleTimeString(), 1000);
+
+
+function PDF() {
+  myWin= open("menu/menuNEW.pdf");
+}
+ 
+
+
+
